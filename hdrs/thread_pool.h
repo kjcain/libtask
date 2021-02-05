2dc646342a4219f92364b174dbb40f88d384f85f
kyler.j.cain2 <kyler.j.cain2.mil@mail.mil>
2021-02-05 09:47:04 -0500

    #3, implemented basic version of libtask

--- hdrs/thread_pool.h
+++ hdrs/thread_pool.h
@@ -29,27 +29,18 @@ typedef void work_func_t(void *params);
 
 /**
  * @brief create a new pool
- * @note auto scaling will increase pool size in an effort to keep task backlog smaller than the number of threads available
- * @note scaling factor is ((max - min) / 10) + 1
  * 
- * @param min_size 
+ * @param size 
  * @return thread_pool_t* 
  */
-thread_pool_t *pool_init(size_t min_size, size_t max_size);
+thread_pool_t *pool_init(size_t size);
 
 /**
- * @brief release all resources associated with the threadpool
+ * @brief wait for all units of work to terminate and free the pool
  * 
  * @param pool 
  */
-void pool_free(thread_pool_t *pool);
-
-/**
- * @brief wait for all units of work to terminate
- * 
- * @param pool 
- */
-void pool_wait(thread_pool_t *pool);
+void pool_flush(thread_pool_t *pool);
 
 /**
  * @brief perform a unit of work on the thread pool
@@ -59,45 +50,6 @@ void pool_wait(thread_pool_t *pool);
  * @param params 
  */
 void pool_do(thread_pool_t *pool, work_func_t *func, void *params);
-
-/**
- * @brief gets the number of allocatee threads in the pool
- * 
- * @param pool 
- * @return size_t 
- */
-size_t pool_get_size(thread_pool_t *pool);
-
-/**
- * @brief get the number of threads that are currently not running in the pool
- * 
- * @param pool 
- * @return size_t 
- */
-size_t pool_get_free_threads(thread_pool_t *pool);
-
-/**
- * @brief get the number of threads that are currently working in the pool
- * 
- * @param pool 
- * @return size_t 
- */
-size_t pool_get_busy_threads(thread_pool_t *pool);
-
-/**
- * @brief get the number of work units waiting to execute
- * 
- * @param pool 
- * @return size_t 
- */
-size_t pool_get_backlog(thread_pool_t *pool);
-
-/**
- * @brief print statistics for the pool
- * 
- * @param pool 
- */
-void pool_print_stats(thread_pool_t *pool);
 
 #endif /* THREAD_POOL_H */
 
--- src/thread_pool.c
+++ src/thread_pool.c
@@ -14,9 +14,199 @@
 #include <signal.h>
 #include <pthread.h>
 #include <stdatomic.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 #include "thread_pool.h"
 
-#define THREAD_POOL_SCALING_FACTOR 10
+#define PTHREAD_SHARED 0
+
+/**
+ * @brief standard unit of work
+ */
+typedef struct _work_t
+{
+    work_func_t *func;
+    void *params;
+
+    struct _work_t *next;
+}
+work_t;
+
+/**
+ * @brief thread pool
+ */
+struct _thread_pool_t 
+{
+    size_t size;
+
+    volatile atomic_int live_threads;
+    volatile atomic_int busy_threads;
+    volatile atomic_int work_backlog;
+
+    sem_t all_dead;
+
+    sem_t work_signal;
+    sem_t work_lock;
+    work_t *work;
+
+    pthread_t threads[];
+};
+
+work_t *_pool_get_task(thread_pool_t *pool)
+{
+    work_t *work;
+
+    sem_wait(&(pool->work_signal));
+    sem_wait(&(pool->work_lock));
+
+    work = pool->work;
+
+    if(work != NULL)
+    {
+        pool->work = pool->work->next;
+        pool->work_backlog--;
+    }
+
+    sem_post(&(pool->work_lock));
+
+    return work;
+}
+
+void _pool_add_task(thread_pool_t *pool, work_func_t *func, void *params)
+{
+    work_t *work = malloc(sizeof(work_t));
+    work->func = func;
+    work->params = params;
+    work->next = NULL;
+
+    sem_wait(&(pool->work_lock));
+
+    if(pool->work == NULL)
+    {
+        pool->work = work;
+    }
+    else
+    {
+        work_t *work_node = pool->work;
+        while(work_node->next != NULL)
+        {
+            work_node = work_node->next;
+        }
+
+        work_node->next = work;
+    }
+
+    pool->work_backlog++;
+
+    sem_post(&(pool->work_lock));
+    sem_post(&(pool->work_signal));
+}
+
+void *_pool_kernel(void *pool_generic)
+{
+    thread_pool_t *pool = (thread_pool_t *) pool_generic;
+
+    bool flush = false;
+    pool->live_threads++;
+
+    while(!flush)
+    {
+        work_t *work = _pool_get_task(pool);
+
+        if(work != NULL)
+        {
+             pool->busy_threads++;
+
+            work->func(work->params);
+
+            free(work);
+
+            pool->busy_threads--;
+        }
+        else
+        {
+            flush = true;
+        }
+    }
+    pool->live_threads--;
+
+    if(pool->live_threads <= 0)
+    {
+        sem_post(&(pool->all_dead));
+    }
+
+    return NULL;
+}
+
+/**
+ * @brief create a new pool
+ * 
+ * @param size 
+ * @return thread_pool_t* 
+ */
+thread_pool_t *pool_init(size_t size)
+{
+    assert(size > 0);
+
+    size_t pool_size = sizeof(thread_pool_t) + (sizeof(pthread_t) * size);
+    thread_pool_t *pool = malloc(pool_size);
+    assert(pool != NULL && "unable to allocate pool");
+
+    pool->size = size;
+    
+    pool->live_threads = 0;
+    pool->busy_threads = 0;
+    pool->work_backlog = 0;
+
+    sem_init(&(pool->all_dead), PTHREAD_SHARED, 0);
+    
+    sem_init(&(pool->work_signal), PTHREAD_SHARED, 0);
+    sem_init(&(pool->work_lock), PTHREAD_SHARED, 1);
+
+    pool->work = NULL;
+
+    for(size_t thread = 0; thread < size; thread++)
+    {
+        pthread_create(&(pool->threads[thread]), NULL, _pool_kernel, pool);
+    }
+
+    return pool;
+}
+
+/**
+ * @brief wait for all units of work to terminate and free the pool
+ * 
+ * @param pool 
+ */
+void pool_flush(thread_pool_t *pool)
+{
+    for(size_t count = 0; count < pool->size; count++)
+    {
+        sem_post(&(pool->work_signal));
+    }
+    
+    sem_wait(&(pool->all_dead));
+
+    for(size_t count = 0; count < pool->size; count++)
+    {
+        pthread_join(pool->threads[count], NULL);
+    }
+
+    free(pool);
+    pool = NULL;
+}
+
+/**
+ * @brief perform a unit of work on the thread pool
+ * 
+ * @param pool 
+ * @param func 
+ * @param params 
+ */
+void pool_do(thread_pool_t *pool, work_func_t *func, void *params)
+{
+    _pool_add_task(pool, func, params);
+}
 
 /* end of task_pool.c */
